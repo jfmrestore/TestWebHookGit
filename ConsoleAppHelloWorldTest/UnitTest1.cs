e097ff27fe3803bb289f2956445a32b0da2cf39e
jfmrestore@gmail.com <jfmrestore@gmail.com>
2021-02-24 09:51:12 +0100

    Initialisation projet

---
+++ ConsoleAppHelloWorldTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConsoleAppHelloWorldTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
